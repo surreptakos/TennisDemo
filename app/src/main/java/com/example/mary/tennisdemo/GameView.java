58827d5679bf9b86a3a83957bcbd609bea5a7ebb
surreptakos <djgatsakos@gmail.com>
2015-12-05 21:02:08 -0300

    SurfaceView

---
+++ app/src/main/java/com/example/mary/tennisdemo/GameView.java
@@ -0,0 +1,12 @@
+package com.example.mary.tennisdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+
+public class GameView extends SurfaceView {
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
