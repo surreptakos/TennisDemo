0f2d129261535bba396521d62d969a8103f6fa7f
surreptakos <djgatsakos@gmail.com>
2016-01-05 18:18:08 -0600

    Randomizing ball direction

--- app/src/main/java/com/example/mary/tennisdemo/Ball.java
+++ app/src/main/java/com/example/mary/tennisdemo/Ball.java
@@ -1,18 +1,22 @@
 package com.example.mary.tennisdemo;
 
 
+import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.util.Log;
 
+import java.util.Random;
+
 public class Ball extends Sprite {
 
+    private static final float V = 0.25f;
     private float vX;
     private float vY;
 
     public Ball(int screenWidth, int screenHeight) {
         super(screenWidth, screenHeight);
-        vX = 0.05f;
-        vY = 0.05f;
+        vX = 0;
+        vY = 0;
     }
 
     public void update(long elapsed) {
@@ -41,5 +45,19 @@ public class Ball extends Sprite {
         setX(x);
         setY(y);
 
+    }
+
+    @Override
+    public void init(Bitmap image, Bitmap shadow) {
+        super.init(image, shadow);
+
+        setX((getScreenWidth() - getRect().width()) / 2);
+        setY((getScreenHeight() - getRect().height()) / 2);
+
+        Random random = new Random();
+
+        float rand = (random.nextFloat() * 2) - 1;
+        vX = V * rand;
+        vY = V * (float) Math.sqrt((double) (1 - (rand * rand)));
     }
 }
